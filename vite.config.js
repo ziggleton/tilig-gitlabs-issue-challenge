e379a110666760276aa5b6f182b536b94399b643
Ziggleton <ziggy@devhipsters.com>
2022-01-30 20:43:48 +0200

    configure scss import for styling

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+const pathSrc = path.resolve(__dirname, './src')
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: { additionalData: `@import "${pathSrc}/assets/scss/index.scss";` },
+    },
+  },
 })
