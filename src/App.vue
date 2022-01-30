4c6610f71255f36041b6d901a82fa7ba79cb64f2
Ziggleton <ziggy@devhipsters.com>
2022-01-30 21:57:54 +0200

    added basic issue list section

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
     <AppHeaderComponent></AppHeaderComponent>
+    <div class="main-content">
+        <h3 class="page-title">Gitlab Issues Emoji</h3>
+        <IssueListComponent></IssueListComponent>
+    </div>
 </template>
 <script setup>
 import AppHeaderComponent from './components/shared/AppHeaderComponent.vue';
+import IssueListComponent from './components/shared/IssueListComponent.vue';
 </script>
 <style lang="scss">
 @import "./assets/scss/index.scss";
---
+++ src/components/shared/IssueListComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="issue-list">
+        <h4 class="issues-title">{{ state.title }}</h4>
+    </section>
+</template>
+
+<script setup>
+import { computed, defineProps, reactive } from 'vue';
+const props = defineProps({
+    title: String,
+    issueList: Array
+})
+
+const state = reactive({
+    title: computed(() => {
+        if (!props.title) return 'Default title'
+    })
+})
+
+</script>
+
+<style lang="scss" scoped>
+.issue-list {
+    margin-top: 16px;
+}
+</style>
